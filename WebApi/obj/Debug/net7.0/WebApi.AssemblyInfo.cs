b7ae415635279839ac9ea4069ddaf240375d0664
jaimeleonc <jaimeadalbertoleon@gmail.com>
2024-10-22 11:36:03 -0600

    guardando proyecto

--- WebApi/obj/Debug/net7.0/WebApi.AssemblyInfo.cs
+++ WebApi/obj/Debug/net7.0/WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0eca419b2fbcfc58cf4dba3273d5b468d4694da8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+983d83de00000cbecafb4f3e390ccc7ad4eddd68")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
